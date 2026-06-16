c4d9eca0514265f1c1a7de6f5a1f2bc0d3f6ec13
ulul0386-hue <ulul0386@gmail.com>
2026-06-16 09:20:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -166,7 +166,7 @@ function performCrossover() {
     // let's average: (Father's egg gene [0] + Mother's eggs) / 2 = Mother's eggs / 2.
     // Or we can assume rooster carries egg gene equivalent to 0. 
     // Let's do (0 + mother.eggs) / 2 as per standard average of parent attributes.
-    const eggs = Math.round((0 + mother.eggs) * 1.5);
+    const eggs = Math.round((0 + mother.eggs) * 2);
     
     state.offspring.push({
       name: `Anak Ayam ${index + 1}`,
